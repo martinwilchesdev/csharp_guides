3ec630323fa58ab5104f16e62f625bace6756752
Martin Wilches <martindotdev@gmail.com>
2024-04-27 11:18:18 -0500

    Metodos y valores retornados

---
+++ TestProject/Program.cs
@@ -0,0 +1,8 @@
+﻿Random dice = new Random();
+var roll1 = dice.Next();
+var roll2 = dice.Next(105);
+var roll3 = dice.Next(100, 200);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
